b3ffd8b5df4c706148b774690c6e5455ee8dfbb3
Petru <smart__petea@mail.ru>
2020-09-17 22:29:46 +0300

    generate state

--- app/handlers/auth.go
+++ app/handlers/auth.go
@@ -7,6 +7,11 @@ import (
     "golang.org/x/oauth2"
     "github.com/spf13/viper"
     "golang.org/x/oauth2/facebook"
+
+    "crypto/rand"
+    "encoding/base64"
+
+    "github.com/gorilla/sessions"
 )
 
 func FacebookOauth2Login(w http.ResponseWriter, r *http.Request) {
@@ -20,9 +25,9 @@ func FacebookOauth2Login(w http.ResponseWriter, r *http.Request) {
         Endpoint:     facebook.Endpoint,
     }
 
-    oauthState := "oauthState" //todo save oauthState. read docs
+    oauthState := generateStateOauthCookie() //todo 1. store it in a session
+    //todo 2. look for a context to be shared through requests
     fbLoginUrl := facebookOauthConfig.AuthCodeURL(oauthState)
-    fmt.Printf("%s\n", fbLoginUrl)
     http.Redirect(w, r, fbLoginUrl, http.StatusTemporaryRedirect)
 }
 
@@ -33,4 +38,11 @@ func FacebookOauth2Callback(w http.ResponseWriter, r *http.Request) {
 
         //todo Create user. What's next?
         w.Write([]byte("facebook callback"))
-    }
+}
+
+func generateStateOauthCookie() string {
+    b := make([]byte, 128)
+    rand.Read(b)
+    state := base64.URLEncoding.EncodeToString(b)
+    return state
+}
