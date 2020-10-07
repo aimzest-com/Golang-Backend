ae5f23016a4cb1342b7ec840da1f1e0cc7220d7a
Petru <smart__petea@mail.ru>
2020-10-07 22:49:25 +0300

    first part verify token

--- app/jwt_storage.go
+++ app/jwt_storage.go
@@ -6,6 +6,9 @@ import (
     "github.com/twinj/uuid"
     "time"
     "strconv"
+    "net/http"
+    "strings"
+    "fmt"
  )
 
 type JWTStorage struct {
@@ -78,4 +81,85 @@ func (storage *JWTStorage) CreateAuth(userId uint64, td *TokenDetails) error {
     }
 
     return nil
+}
+
+func verifyToken(r *http.Request, accessSecret string) (*jwt.Token, error) {
+    tokenString := extractToken(r)
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+        }
+        return []byte(accessSecret), nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    return token, nil
+}
+
+func tokenValid(r *http.Request, accessSecret string) error {
+    token, err := verifyToken(r, accessSecret)
+    if err != nil {
+        return err
+    }
+
+    if _, ok := token.Claims.(jwt.Claims); !ok || !token.Valid {
+        return err //todo something wrong with this error
+    }
+
+    return nil
+}
+
+func extractToken(r *http.Request) string {
+    bearToken := r.Header.Get("Authorization")
+    strArr := strings.Split(bearToken, " ")
+    if len(strArr) == 2 {
+        return strArr[1]
+    }
+
+    return ""
+}
+
+func extractTokenMetadata(r *http.Request, accessSecret string) (*AccessDetails, error) {
+    token, err := verifyToken(r, accessSecret)
+    if err != nil {
+        return nil, err
+    }
+
+    claims, ok := token.Claims.(jwt.MapClaims)
+    if ok && token.Valid {
+        accessUuid, ok := claims["access_uuid"].(string)
+        if !ok {
+            return nil, err
+        }
+
+        userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+        if err != nil {
+            return nil, err
+        }
+
+        return &AccessDetails {
+            AccessUuid: accessUuid,
+            UserId: userId,
+        }, nil
+    }
+
+    return nil, err
+}
+
+type AccessDetails struct {
+    AccessUuid string
+    UserId uint64
+}
+
+func (storage *JWTStorage) FetchAuth(authD *AccessDetails) (uint64, error) {
+    userid, err := storage.Client.Get(authD.AccessUuid).Result()
+    if err != nil {
+        return 0, err
+    }
+
+    userID, _ := strconv.ParseUint(userid, 10, 64)
+    return userID, nil
 }
